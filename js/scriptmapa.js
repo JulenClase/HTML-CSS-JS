36a65ceb6380bec9989bdfb51c42cb122a283220
Julen R.Costa <julencosasdeclase@gmail.com>
2016-12-20 17:58:43 +0100

    commit 20/12/2016

--- js/scriptmapa.js
+++ js/scriptmapa.js
@@ -18,9 +18,9 @@ jQuery(document).ready(function($) {
         var directionsDisplay = new google.maps.DirectionsRenderer({
             draggable: true
         });
-        directionsDisplay.addListener('directions_changed', function() {
+        /*directionsDisplay.addListener('directions_changed', function() {
             computeTotalDistance(directionsDisplay.getDirections());
-        });
+        });*/
 
         var element = document.getElementById('map');
         var myCenter = new google.maps.LatLng(coordenadas.latitude, coordenadas.longitude);
@@ -29,30 +29,43 @@ jQuery(document).ready(function($) {
             zoom: 19,
             scrollwheel: true
         };
-        var infowindow = new google.maps.InfoWindow({
+        /*var infowindow = new google.maps.InfoWindow({
             content: "Posicion Actual."
-        });
+        });*/
         var map = new google.maps.Map(element, mapOptions);
         directionsDisplay.setMap(map);
         directionsDisplay.setPanel(document.getElementById('right-panel'));
 
-        var marker = new google.maps.Marker({position: myCenter});
-        marker.setMap(map);
+        var marker = new google.maps.Marker({
+            position: myCenter,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+        //marker.addListener('click', toggleBounce);
+
+        //marker.setMap(map);
 
         var poscasa = {lat:43.2334945, lng:-2.8537471}
-        var marker2 = new google.maps.Marker({position:poscasa});
+        var marker2 = new google.maps.Marker({
+            position:poscasa,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+        //marker2.addListener('click', toggleBounce);
         //marker2.setMap(map);
 
         calculateAndDisplayRoute(directionsService, directionsDisplay , myCenter, poscasa);
 
-        infowindow.open(map, marker, marker2);
+        //infowindow.open(map, marker);
     }
 
     function calculateAndDisplayRoute(directionsService, directionsDisplay, myCenter, poscasa) {
+        //console.log(directionsService);
         directionsService.route({
             origin: myCenter,
             destination: poscasa,
-            travelMode: 'DRIVING'
+            travelMode: 'DRIVING',
+
         }, function(response, status) {
             if (status === 'OK') {
                 directionsDisplay.setDirections(response);
@@ -67,4 +80,13 @@ jQuery(document).ready(function($) {
         .catch(function errorHandler(error) {
             console.log(error);
         });
+
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        }
+    }
 });
